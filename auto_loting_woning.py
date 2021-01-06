3e8752501d67fe39646b370550708941140d791a
sebus208 <kovewebdesign@gmail.com>
2021-01-06 23:41:13 +0100

    Decline cookies & login

--- auto_loting_woning.py
+++ auto_loting_woning.py
@@ -7,3 +7,26 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 GECKO_PATH = "/home/kove/code/python/drivers/geckodriver"
+CHROME_PATH = "/home/kove/code/python/drivers/chromedriver"
+
+WONINGNET = "https://www.woningnetregioamsterdam.nl/Inloggen"
+
+
+def no_cookies():
+    b.find_element_by_css_selector(".cc-cookie-decline").click()
+    time.sleep(3)
+    b.get(WONINGNET)
+
+
+def login():
+    b.find_element_by_id("gebruikersnaam").send_keys(config.username)
+    b.find_element_by_id("password").send_keys(config.password)
+    b.find_element_by_id("loginButton").click()
+
+
+b = webdriver.Chrome(CHROME_PATH)
+b.get(WONINGNET)
+b.maximize_window()
+
+no_cookies()
+login()
