ff3eeae79f031d94ab438157cf0cb7b452d5200e
sebus208 <kovewebdesign@gmail.com>
2021-01-07 00:35:20 +0100

    Open and react on a loting basic loop

--- auto_loting_woning.py
+++ auto_loting_woning.py
@@ -9,13 +9,17 @@ from selenium.webdriver.support import expected_conditions as EC
 GECKO_PATH = "/home/kove/code/python/drivers/geckodriver"
 CHROME_PATH = "/home/kove/code/python/drivers/chromedriver"
 
-WONINGNET = "https://www.woningnetregioamsterdam.nl/Inloggen"
+LOGIN = "https://www.woningnetregioamsterdam.nl/Inloggen"
+LOTING = "https://www.woningnetregioamsterdam.nl/Zoeken#model[Loting]~sort[kamersmax,DESC]"
 
 
-def no_cookies():
+def noCookies():
     b.find_element_by_css_selector(".cc-cookie-decline").click()
-    time.sleep(3)
-    b.get(WONINGNET)
+    time.sleep(2)
+    b.find_element_by_css_selector(".growl-notification .close").click()
+    time.sleep(2)
+    b.get(LOGIN)
+    time.sleep(2)
 
 
 def login():
@@ -24,9 +28,36 @@ def login():
     b.find_element_by_id("loginButton").click()
 
 
+def reagerenLoting(b):
+    reageren_button = ".interactionColumn .primary.button"
+    b.find_element_by_css_selector(reageren_button).click()
+    time.sleep(3)
+
+    b.find_element_by_css_selector(".tabMenuContainer dd:not(.active)").click()
+    b.find_element_by_css_selector("#akkoordContainer label").click()
+    b.find_element_by_id("formsubmitbutton").click()
+    time.sleep(5)
+
+
+def openLotingen():
+    b.get(LOTING)
+    time.sleep(5)
+    unit_selector = ".searchContainer .content .unitContainer > a.unitLink:first-of-type"
+    units = b.find_elements_by_css_selector(unit_selector)
+
+    for unit in units:
+        link = unit.get_attribute("href")
+        b.execute_script("window.open('" + link + "', '_blank');")
+        b.switch_to.window(b.window_handles[1])
+        time.sleep(3)
+        reagerenLoting(b)
+        b.close()
+
+
 b = webdriver.Chrome(CHROME_PATH)
-b.get(WONINGNET)
+b.get(LOGIN)
 b.maximize_window()
 
-no_cookies()
+noCookies()
 login()
+openLotingen()
