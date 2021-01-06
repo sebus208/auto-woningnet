a4999a59947055c528f0bbcff68d631ab6bcf6e7
sebus208 <kovewebdesign@gmail.com>
2021-01-06 23:18:45 +0100

    Created project files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Auto Loting Woning
+
+A bot that automates the process of applying for a loting woning on WoningNet.
---
+++ auto_loting_woning.py
@@ -0,0 +1,9 @@
+import config
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+GECKO_PATH = "/home/kove/code/python/drivers/geckodriver"
